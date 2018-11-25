bc06f0fa3b9bae000c8075ae0d2d897d09e46da8
learncloud <learcloud9@gmail.com>
2018-11-25 13:43:46 +0530

    updates

--- 02sgmain.tf
+++ 02sgmain.tf
@@ -5,11 +5,12 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0bbe6b35405ecebdb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
     #!/bin/bash
-    echo "Hello, World" > index.html
-    nohup busybox httpd -f -p 8080 &
+    echo "Hello, Terraform" > index.html
+    nohup busybox httpd -f -p "${var.server_port}" &
     EOF
 
   tags {
@@ -17,13 +18,25 @@ resource "aws_instance" "example" {
     }
   }
 
+variable "server_port" {
+  description = "Web port for http requests"
+  default = 8080
+  type = "string" 
+}
+
+
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-      from_port = 8080
-      to_port   = 8080
+      from_port = "${var.server_port}"
+      to_port   = "${var.server_port}"
       protocol  = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
       }
     }
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+
+}
